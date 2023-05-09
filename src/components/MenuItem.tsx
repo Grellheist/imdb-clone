b848516e2456393051c00517366904e0f620574a
Grellheist <grellheist@tutanota.com>
2023-05-09 16:07:34 -0300

    Finished Header

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -11,9 +11,9 @@ type MenuItem = {
 export default function MenuItem({ title, address, Icon }: MenuItem) {
     return (
         <div>
-            <Link href={address} className="">
-                <Icon />
-                <p>{title}</p>
+            <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
+                <Icon className="text-2xl sm:hidden mx-4" />
+                <p className="hidden sm:inline my-2 text-sm">{title}</p>
             </Link>
         </div>
     )
