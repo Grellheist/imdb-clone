217dd9d8bd9ef6bf7bfdb1cb14a65516222e4e51
Grellheist <grellheist@tutanota.com>
2023-05-10 13:31:13 -0300

    Fixed alingment on the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div className='max-w-6xl mx-auto space-y-4'>
+        <div className='max-w-6xl mx-auto space-y-4 p-4'>
             <h1 className="text-2xl font-medium text-amber-600">About</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores inventore incidunt magnam quaerat, optio itaque magni laborum repudiandae commodi consequuntur dolores quidem ducimus corporis nam recusandae sed, a temporibus iure!</p>
             <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
